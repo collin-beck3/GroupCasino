76a3ff7528bcf17a40822d221b4d39968796e585
gitanitraj <anitra.com@gmail.com>
2026-05-04 19:36:14 -0400

    CasinoAcct added

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -5,5 +5,41 @@ package com.github.zipcodewilmington.casino;
  * `ArcadeAccount` is registered for each user of the `Arcade`.
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
+
 public class CasinoAccount {
+
+    private String playerName;
+    private double balance;
+
+    public CasinoAccount(String playerName, double startingBalance) {
+        this.playerName = playerName;
+        this.balance = startingBalance;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+    
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true; 
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return playerName + " | Balance: $" + String.format("%.2f", balance);
+    }
 }
