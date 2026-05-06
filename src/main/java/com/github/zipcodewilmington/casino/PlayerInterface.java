e6631137edc4a1a328138fd957e912b6dbcbc6ba
prachi0716 <prachiptl1707@gmail.com>
2026-05-06 16:39:08 -0400

    ignore

--- src/main/java/com/github/zipcodewilmington/casino/PlayerInterface.java
+++ src/main/java/com/github/zipcodewilmington/casino/PlayerInterface.java
@@ -1,11 +1,10 @@
 package com.github.zipcodewilmington.casino;
 
-import com.github.zipcodewilmington.CasinoAccount;
-
 /**
  * Created by leon on 7/21/2020.
  * All players of a game should abide by `PlayerInterface`.
- * All players must have reference to the `ArcadeAccount` used to log into the `Arcade` system.
+ * All players must have reference to the `ArcadeAccount` used to log into the
+ * `Arcade` system.
  * All players are capable of `play`ing a game.
  */
 
@@ -23,6 +22,7 @@ public interface PlayerInterface {
 
     /**
      * Defines how a specific implementation of PlayerInterface plays their game.
+     * 
      * @param <SomeReturnType> specify any return type you would like
      * @return whatever return value you would like
      */
