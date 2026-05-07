77ec95d66e8b24e8983469e2cb939d44c44aabff
gitanitraj <anitra.com@gmail.com>
2026-05-07 13:57:03 -0400

    Updated NumberGuessGameTest

--- src/test/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGameTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGameTest.java
@@ -1,12 +1,12 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
 
